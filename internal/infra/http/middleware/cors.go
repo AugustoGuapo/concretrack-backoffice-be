0ae8e89f069108abbfc364f36baaeb0c7ae2b8e6
Augusto Diaz <adiazpolo17@correo.unicordoba.edu.co>
2025-11-28 11:35:44 -0500

    fixed origins

--- internal/infra/http/middleware/cors.go
+++ internal/infra/http/middleware/cors.go
@@ -8,15 +8,9 @@ import (
 
 func CORS(next http.Handler) http.Handler {
     return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		allowedOrigins := map[string]bool {
-			"http://localhost:5173": true,
-			"http://192.168.1.10:5173": true,
-		}
+
 		origin := r.Header.Get("origin")
-		fmt.Println(origin)
-		if allowedOrigins[origin] {
-			w.Header().Set("Access-Control-Allow-Origin", origin)
-		}
+		w.Header().Set("Access-Control-Allow-Origin", origin)
         w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
         w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		w.Header().Set("Access-Control-Expose-Headers", "Content-Disposition")
